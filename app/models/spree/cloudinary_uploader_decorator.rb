4a4f5358cd919fd33d5097a5c857ee76cbbb3444
Nassim Mezouar mezouar.nass@gmail.com <mezouar.nass@gmail.com>
2019-01-22 21:14:20 +0100

    setup accueil=>navbar

--- app/models/spree/cloudinary_uploader_decorator.rb
+++ app/models/spree/cloudinary_uploader_decorator.rb
@@ -1,5 +1,5 @@
-CloudinaryUploader.class_eval do
-  version :small do
-    process :resize_to_fit => [48, 48]
-  end
-end
+# CloudinaryUploader.class_eval do
+#   version :small do
+#     process :resize_to_fit => [48, 48]
+#   end
+# end
