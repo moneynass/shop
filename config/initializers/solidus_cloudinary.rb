81b98e0830d6cb98c1a849e17439d85e1544f197
Nassim Mezouar mezouar.nass@gmail.com <mezouar.nass@gmail.com>
2019-01-22 11:21:17 +0100

    setup accueil/bootstrap view

---
+++ config/initializers/solidus_cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dszqyk3yi'
+  config.api_key = '886351167219136'
+  config.api_secret = 'PdSjS7tpLE_OXCiTi5107fHF9S8'
+  config.cdn_subdomain = true
+end
